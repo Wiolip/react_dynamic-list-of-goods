8ee9c92252cb354c764501975cad954856754378
Wioleta Lip <wiolip@hotmail.com>
2026-02-16 21:41:49 +0100

    soulution 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 import * as goodsAPI from './api/goods';
@@ -6,24 +6,52 @@ import { Good } from './types/Good';
 
 export const App: React.FC = () => {
   const [goods, setGoods] = React.useState<Good[]>([]);
+  const [error, setError] = useState<string | null>(null);
 
-  const loadAll = async () => {
-    const allGoods = await goodsAPI.getAll();
+  const loadAll = useCallback(async () => {
+    try {
+      setError(null);
+      const allGoods = await goodsAPI.getAll();
 
-    setGoods(allGoods);
-  };
+      setGoods(allGoods);
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError('Something went wrong');
+      }
+    }
+  }, []);
 
-  const loadFirstFive = async () => {
-    const firstFive = await goodsAPI.get5First();
+  const loadFirstFive = useCallback(async () => {
+    try {
+      setError(null);
+      const firstFive = await goodsAPI.get5First();
 
-    setGoods(firstFive);
-  };
+      setGoods(firstFive);
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError('Something went wrong');
+      }
+    }
+  }, []);
 
-  const loadRed = async () => {
-    const redGoods = await goodsAPI.getRedGoods();
+  const loadRed = useCallback(async () => {
+    try {
+      setError(null);
+      const redGoods = await goodsAPI.getRedGoods();
 
-    setGoods(redGoods);
-  };
+      setGoods(redGoods);
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError('Something went wrong');
+      }
+    }
+  }, []);
 
   return (
     <div className="App">
@@ -40,6 +68,8 @@ export const App: React.FC = () => {
       <button type="button" data-cy="red-button" onClick={loadRed}>
         Load red goods
       </button>
+
+      {error && <p style={{ color: 'red' }}>{error}</p>}
 
       <GoodsList goods={goods} />
     </div>
